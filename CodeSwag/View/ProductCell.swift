a500cbe7d90eb6e967e7b0d00c19005bd192f4c2
shalu scaria <shalu.scaria3@gmail.com>
2018-08-10 14:41:53 -0700

    collection view

---
+++ CodeSwag/View/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  CodeSwag
+//
+//  Created by Shalu Scaria on 2018-08-10.
+//  Copyright © 2018 Shalu Scaria. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage : UIImageView!
+    @IBOutlet weak var productTitle : UILabel!
+    @IBOutlet weak var productPrice : UILabel!
+    
+}
